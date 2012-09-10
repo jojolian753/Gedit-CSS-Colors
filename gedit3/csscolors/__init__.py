10a7d7e95860a81f8897f10ba27c1f1cf6fad8b0
Joe R. Nassimian <nassimian.joseph@gmail.com>
2012-09-10 21:56:31 +0200

    Fixed minor glitches

--- gedit3/csscolors/__init__.py
+++ gedit3/csscolors/__init__.py
@@ -33,7 +33,7 @@ class CSSColorsPlugin(GObject.Object, Gedit.ViewActivatable):
         all_colors_re = Colors_Hex_RE
 
         all_colors_re += r"|".join(colors.keys())
-        self.__colors_re = re.compile(all_colors_re)
+        self.__colors_re = re.compile(all_colors_re, re.IGNORECASE)
 
         self.doc = self.view.get_buffer()
 
@@ -50,29 +50,17 @@ class CSSColorsPlugin(GObject.Object, Gedit.ViewActivatable):
                 self.do_language_notify
             )
 
-    def do_deactivate(self):
-#        if self.lang_handler:
-#            self.view.disconnect(self.lang_handler)
-        pass
-
-
     def do_document_changed(self, view):
-        print("CSSColors: document changed")
         self.on_update_active(self.doc)
 
     def do_language_notify(self, view, pspec):
-        print("CSSColors: language changed")
-
         if self.__is_css_doc():
-            print("notify: document is css and is editable")
             self.update_handler = \
                 self.doc.connect(
                     "changed",
                     self.do_document_changed
                 )
             self.__colorify(self.doc)
-        elif self.lang_handler:
-                self.view.disconnect(self.lang_handler)
 
     # -------------------------------------------------------------------------
 
@@ -85,24 +73,16 @@ class CSSColorsPlugin(GObject.Object, Gedit.ViewActivatable):
         return self.doc.get_mime_type() == "text/css"
 
     def __add_tag_if_not_exists(self, tag):
-        print("adding tag if it doesn't exist")
-
         if self.doc.get_tag_table().lookup(tag) == None:
             self.doc.create_tag(tag, background=tag)
 
     def __colorify(self, doc):
-        print("colorifying")
-
         astart, anend = doc.get_bounds()
         self.__colorify_range(doc, astart, anend)
 
     def __colorify_range(self, doc, i_start, i_end):
-        print("colorifying range")
-
         txt = doc.get_text(i_start, i_end, False)
-        print("text is {0}".format(txt))
         for m in self.__colors_re.finditer(txt):
-            print("m is {0}".format(m))
             start = doc.get_iter_at_offset(
                 i_start.get_offset() + m.start()
             )
@@ -111,15 +91,14 @@ class CSSColorsPlugin(GObject.Object, Gedit.ViewActivatable):
                 i_start.get_offset() + m.end()
             )
 
-            print("m.group(0) is {0}".format(m.group(0)))
-            self.__add_tag_if_not_exists(m.group(0))
-            self.doc.apply_tag_by_name(m.group(0), start, end)
+            color_code = m.group(0)
+            if not color_code.startswith("#"):
+                color_code = colors[color_code.lower()]
 
-    # -------------------------------------------------------------------------
+            self.__add_tag_if_not_exists(color_code)
+            self.doc.apply_tag_by_name(color_code, start, end)
 
     def on_update_active(self, doc):
-        print("updating active")
-
         rmtags = lambda tag, _: doc.remove_tag(tag, start, end)
 
         end = doc.get_iter_at_mark(doc.get_insert())
